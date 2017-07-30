aa80f84be1ac1b5817fd81b34b97f98b8db1440c
Lam TANG <tanglam77@gmail.com>
2017-07-30 21:03:02 +0200

    update

--- ghost 1.0.2/docker-entrypoint.sh
+++ ghost 1.0.2/docker-entrypoint.sh
@@ -2,27 +2,13 @@
 set -e
 
 # allow the container to be started with `--user`
-if [[ "$*" == npm*start* ]] && [ "$(id -u)" = '0' ]; then
-	chown -R node "$GHOST_CONTENT"
+if [[ "$*" == node*current/index.js* ]] && [ "$(id -u)" = '0' ]; then
+	chown -R node "$GHOST_INSTALL"
 	exec su-exec node "$BASH_SOURCE" "$@"
 fi
 
-if [[ "$*" == npm*start* ]]; then
-	baseDir="$GHOST_SOURCE/content"
-	for dir in "$baseDir"/*/ "$baseDir"/themes/*/; do
-		targetDir="$GHOST_CONTENT/${dir#$baseDir/}"
-		mkdir -p "$targetDir"
-		if [ -z "$(ls -A "$targetDir")" ]; then
-			tar -c --one-file-system -C "$dir" . | tar xC "$targetDir"
-		fi
-	done
-
-	if [ ! -e "$GHOST_CONTENT/config.js" ]; then
-		sed -r '
-			s/127\.0\.0\.1/0.0.0.0/g;
-			s!path.join\(__dirname, (.)/content!path.join(process.env.GHOST_CONTENT, \1!g;
-		' "$GHOST_SOURCE/config.example.js" > "$GHOST_CONTENT/config.js"
-	fi
+if [[ "$*" == node*current/index.js* ]]; then
+	knex-migrator-migrate --init --mgpath "$GHOST_INSTALL/current"
 fi
 
 exec "$@"
